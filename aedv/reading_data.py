30b081b73edcb2aadecc38dfbeaef307eff0b2a2
g4briwlle <gabriellemascarelo@gmail.com>
2025-04-21 20:33:25 -0300

    organizing acess to paths in reading_data.py

--- aedv/reading_data.py
+++ aedv/reading_data.py
@@ -1,4 +1,8 @@
 from pathlib import Path
 
-DATA_DIR = Path().resolve().parent / 'data'
+DATA_DIR = Path(__file__).resolve().parent.parent / 'data'
 df_meses_2017_mandioqueiro = DATA_DIR / 'df_meses_2017_mandioqueiro'
+historial_cambios_me_epp_solo = DATA_DIR / "historial_cambios_me_epp_solo.csv"
+
+if __name__ == "__main__":
+    print(df_meses_2017_mandioqueiro, DATA_DIR)
