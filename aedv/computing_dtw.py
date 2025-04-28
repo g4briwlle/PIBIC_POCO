5da9c6bbf19305705f8dbec7056877f1d12bf439
g4briwlle <gabriellemascarelo@gmail.com>
2025-04-27 21:24:01 -0300

    computando matriz de distancias e fazendo o clustering com o dbscan nas 55 emps - falho

--- aedv/computing_dtw.py
+++ aedv/computing_dtw.py
@@ -5,31 +5,36 @@
 
 from fastdtw import fastdtw
 from scipy.spatial.distance import euclidean
+from tslearn.metrics import cdist_dtw
+from sklearn.cluster import DBSCAN
 import pandas as pd
 import numpy as np
+import reading_data
 from valid_time_s import *
 
 # Usando o df de emps que trocaram de nome ao longo dos 11 anos (55 emps)
-df_hist_cambios = pd.read_csv(r'data\historial_cambios_me_epp_solo.csv')
-emps_suspicious = df_hist_cambios["CPF_CNPJ_Rem"]
-emps_suspicious.rename(columns = {"CPF_CNPJ_Rem": "Empresa"})
-
-df_filtrado_entrada = df_entrada[df_entrada['Empresa'].isin(emps_suspicious)]
+df_hist_cambios = pd.read_csv(reading_data.historial_cambios_me_epp_solo)
+emps_suspicious = df_hist_cambios[["CPF_CNPJ_Rem"]].rename(columns = {"CPF_CNPJ_Rem": "Empresa"})
     
 df_time_s_entrada["Empresa"] = df_time_s_entrada["Empresa"].astype(int)
 emps_suspicious = emps_suspicious.astype(int)
 
-distancias = []
-for index, emp1 in enumerate(emps_suspicious):
-    if index < len(emps_suspicious) - 1:
-        for emp2 in emps_suspicious[index + 1:]:
-            if emp1 != emp2:
-                serie_emp1 = df_time_s_entrada[df_time_s_entrada["Empresa"] == emp1]["Volume_Saida"].values
-                serie_emp2 = df_time_s_entrada[df_time_s_entrada["Empresa"] == emp2]["Volume_Saida"].values
-                serie_emp1 = serie_emp1.reshape(-1, 1)
-                serie_emp2 = serie_emp2.reshape(-1, 1)
-                if serie_emp1.size > 0 and serie_emp2.size > 0:
-                    # Mantém como arrays NumPy
-                    distance, path = fastdtw(serie_emp1.reshape(-1, 1), serie_emp2.reshape(-1, 1), dist=euclidean)
-                    distancias.append(distance)
-print(distancias)
+def calculate_dtw(time_series_passed: pd.DataFrame):
+    # converting df to array to use in cdist_dtw
+    series_array = time_series_passed.values.astype(float)
+    # calculating cross similarity matrix
+    distance_matrix = cdist_dtw(series_array)
+    return distance_matrix
+
+distance_matrix = calculate_dtw(segundo_df_time_s_entrada)
+print(distance_matrix, distance_matrix)
+
+dbscan_object = DBSCAN(
+    eps=25,
+    metric="precomputed",
+    min_samples=3
+)
+
+labels = dbscan_object.fit_predict(distance_matrix)
+print(labels)
+
